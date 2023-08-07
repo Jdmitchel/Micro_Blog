75908bb4f21a57cc6fd032cedb1d3e681ec60bec
Jardel - JD <mitchelljardel@gmail.com>
2023-08-07 14:47:46 -0400

    python code

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+entries = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        entry_content = request.form.get("blog-data")
+        formatted = datetime.datetime.today().strftime("[%Y/%m/%d]")
+        print(entry_content, formatted)
     return render_template("home.html")
+
 
 
 if __name__ == '__main__':
