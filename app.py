9addeb3b76e52fdac13cc721b1eed9fb79f9d5de
Jardel - JD <mitchelljardel@gmail.com>
2023-08-08 14:32:10 -0400

    issue fixed

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 import datetime
+import os
 from flask import Flask, render_template, request
 from pymongo import MongoClient
-
-app = Flask(__name__)
-client = MongoClient('mongodb+srv://maisonjardel:Ah9LLB6MTONFUUSk@webdev.nqkzwyx.mongodb.net/') # connect to mongodb server
-app.db = client.MicroBlog
+from dotenv import load_dotenv
 
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    #print([e for e in app.db.Webdev.find({})])
-    if request.method == "POST":
-        entry_content = request.form.get("blog-data")
-        formatted = datetime.datetime.today().strftime("%Y/%m/%d")
-        #entries.append((entry_content, formatted))
-        app.db.Webdev.insert_one({"content": entry_content, "date": formatted})
+load_dotenv()
 
-    entries_dates = [
-        (entry["content"], 
-         entry["date"], 
-         datetime.datetime.strptime(entry["date"], "%Y/%m/%d").strftime("%b %d"))
-    for entry in app.db.Webdev.find({})
-    ]
+def create_app():
 
-    return render_template("home.html", entries = entries_dates)
+    app = Flask(__name__)
+    client = MongoClient(os.getenv ("MONGODB_URI")) # connect to mongodb server
+    app.db = client.MicroBlog
 
 
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        #print([e for e in app.db.Webdev.find({})])
+        if request.method == "POST":
+            entry_content = request.form.get("blog-data")
+            formatted = datetime.datetime.today().strftime("%Y/%m/%d")
+            #entries.append((entry_content, formatted))
+            app.db.Webdev.insert_one({"content": entry_content, "date": formatted})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        entries_dates = [
+            (entry["content"], 
+            entry["date"], 
+            datetime.datetime.strptime(entry["date"], "%Y/%m/%d").strftime("%b %d"))
+        for entry in app.db.Webdev.find({})
+        ]
+
+        return render_template("home.html", entries = entries_dates)
+    
+    return app
